59c214cf801950d5caff0c6f1eed269307532473
Ehsan Hadavand <ehsanhadavand1994@gmail.com>
2023-07-28 17:26:06 +0330

    Send JsonResponse from view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World!")
+    return JsonResponse({"title":"blog",
+                         "content":"this is sample content",
+                         "data":{"name":"Saul",
+                                 "age":29,
+                                 "location":"USA"}})
